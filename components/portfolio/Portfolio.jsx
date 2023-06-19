d9b5e896e07eeaa8124c172a6b0acb3cf08608cc
binhdocao <binhdocao@tamu.edu>
2023-06-19 17:09:06 -0500

    updated portfolio page - ivan/amelia/blake/shikhar

--- components/portfolio/Portfolio.jsx
+++ components/portfolio/Portfolio.jsx
@@ -57,14 +57,256 @@ const Portfolio = () => {
                 data-aos="fade-right"
                 data-aos-duration="1200"
               >
-         <li>
+         
+                {/* START OF IMAGE 5 - Amelia */}
+                <li>
+                  <div className="inner">
+                    <div className="entry tokyo_tm_portfolio_animation_wrap">
+                      <Item
+                        original="/img/portfolio/amelia.jpg"
+                        thumbnail="/img/portfolio/amelia.jpg"
+                        width={600}
+                        height={900}
+                      >
+                        {({ ref, open }) => (
+                          <Image
+                            srl_gallery_image="true"
+                            src="/img/portfolio/amelia.jpg"
+                            alt="Amelia"
+                            data-tip
+                            data-for="Amelia"
+                            role="button"
+                            ref={ref}
+                            onClick={open}
+                          />
+                        )}
+                      </Item>
+                      <ReactTooltip
+                        id="Amelia"
+                        place="bottom"
+                        type="light"
+                        effect="float"
+                        className="tooltip-wrapper"
+                      >
+                        <div>
+                          <h5>Amelia</h5>
+                          <span></span>
+                        </div>
+                      </ReactTooltip>
+                    </div>
+                  </div>
+                </li>
+                {/* END SHOT */}
+                
+                {/* START OF IMAGE 2 - CORD */}
+                <li>
+                  <div className="inner">
+                    <div className="entry tokyo_tm_portfolio_animation_wrap">
+                      <Item
+                        original="/img/portfolio/2.jpg"
+                        thumbnail="/img/portfolio/2.jpg"
+                        width={1200}
+                        height={800}
+                      >
+                        {({ ref, open }) => (
+                          <Image
+                            srl_gallery_image="true"
+                            src="/img/portfolio/2.jpg"
+                            alt="Cord"
+                            data-tip
+                            data-for="cord"
+                            role="button"
+                            ref={ref}
+                            onClick={open}
+                          />
+                        )}
+                      </Item>
+                      <ReactTooltip
+                        id="cord"
+                        place="bottom"
+                        type="light"
+                        effect="float"
+                        className="tooltip-wrapper"
+                      >
+                        <div>
+                          <h5>Cord</h5>
+                          <span></span>
+                        </div>
+                      </ReactTooltip>
+                    </div>
+                  </div>
+                </li>
+                {/* END SHOT */}
+
+                {/* START OF IMAGE 8 - Shikhar */}
+                <li>
+                  <div className="inner">
+                    <div className="entry tokyo_tm_portfolio_animation_wrap">
+                      <Item
+                        original="/img/portfolio/shikhar.jpg"
+                        thumbnail="/img/portfolio/shikhar.jpg"
+                        width={600}
+                        height={900}
+                      >
+                        {({ ref, open }) => (
+                          <Image
+                            srl_gallery_image="true"
+                            src="/img/portfolio/shikhar.jpg"
+                            alt="Shikhar"
+                            data-tip
+                            data-for="Shikhar"
+                            role="button"
+                            ref={ref}
+                            onClick={open}
+                          />
+                        )}
+                      </Item>
+                      <ReactTooltip
+                        id="Shikhar"
+                        place="bottom"
+                        type="light"
+                        effect="float"
+                        className="tooltip-wrapper"
+                      >
+                        <div>
+                          <h5>Shikhar</h5>
+                          <span></span>
+                        </div>
+                      </ReactTooltip>
+                    </div>
+                  </div>
+                </li>
+                {/* END SHOT */}
+
+                {/* START OF IMAGE 4 - BRENDAN */}
+                <li>
+                  <div className="inner">
+                    <div className="entry tokyo_tm_portfolio_animation_wrap">
+                      <Item
+                        original="/img/portfolio/brendan.jpg"
+                        thumbnail="/img/portfolio/brendan.jpg"
+                        width={600}
+                        height={900}
+                      >
+                        {({ ref, open }) => (
+                          <Image
+                            srl_gallery_image="true"
+                            src="/img/portfolio/brendan.jpg"
+                            alt="Childhood"
+                            data-tip
+                            data-for="brendan"
+                            role="button"
+                            ref={ref}
+                            onClick={open}
+                          />
+                        )}
+                      </Item>
+                      <ReactTooltip
+                        id="brendan"
+                        place="bottom"
+                        type="light"
+                        effect="float"
+                        className="tooltip-wrapper"
+                      >
+                        <div>
+                          <h5>Brendan</h5>
+                          <span></span>
+                        </div>
+                      </ReactTooltip>
+                    </div>
+                  </div>
+                </li>
+                {/* END SHOT */}
+
+                {/* START OF IMAGE 6 - Blake */}
+                <li>
+                  <div className="inner">
+                    <div className="entry tokyo_tm_portfolio_animation_wrap">
+                      <Item
+                        original="/img/portfolio/blake.jpg"
+                        thumbnail="/img/portfolio/blake.jpg"
+                        width={1200}
+                        height={800}
+                      >
+                        {({ ref, open }) => (
+                          <Image
+                            srl_gallery_image="true"
+                            src="/img/portfolio/blake.jpg"
+                            alt="Blake"
+                            data-tip
+                            data-for="Blake"
+                            role="button"
+                            ref={ref}
+                            onClick={open}
+                          />
+                        )}
+                      </Item>
+                      <ReactTooltip
+                        id="Blake"
+                        place="bottom"
+                        type="light"
+                        effect="float"
+                        className="tooltip-wrapper"
+                      >
+                        <div>
+                          <h5>Blake</h5>
+                          <span></span>
+                        </div>
+                      </ReactTooltip>
+                    </div>
+                  </div>
+                </li>
+                {/* END SHOT */}
+
+                {/* START OF IMAGE 7 - Ivan */}
+                <li>
+                  <div className="inner">
+                    <div className="entry tokyo_tm_portfolio_animation_wrap">
+                      <Item
+                        original="/img/portfolio/ivan.jpg"
+                        thumbnail="/img/portfolio/ivan.jpg"
+                        width={600}
+                        height={900}
+                      >
+                        {({ ref, open }) => (
+                          <Image
+                            srl_gallery_image="true"
+                            src="/img/portfolio/ivan.jpg"
+                            alt="Ivan"
+                            data-tip
+                            data-for="Ivan"
+                            role="button"
+                            ref={ref}
+                            onClick={open}
+                          />
+                        )}
+                      </Item>
+                      <ReactTooltip
+                        id="Ivan"
+                        place="bottom"
+                        type="light"
+                        effect="float"
+                        className="tooltip-wrapper"
+                      >
+                        <div>
+                          <h5>Ivan</h5>
+                          <span></span>
+                        </div>
+                      </ReactTooltip>
+                    </div>
+                  </div>
+                </li>
+                {/* END SHOT */}
+
+                  {/* START OF IMAGE 1 - CHARLIE */}
+                  <li>
                   <div className="inner">
                     <div className="entry tokyo_tm_portfolio_animation_wrap">
                       <Item
                         original="/img/portfolio/5.jpg"
                         thumbnail="/img/portfolio/5.jpg"
-                        width={600}
-                        height={400}
+                        width={1200}
+                        height={800}
                       >
                         {({ ref, open }) => (
                           <Image
@@ -95,56 +337,16 @@ const Portfolio = () => {
                   </div>
                 </li>
                 {/* END SHOT */}
-
-                <li>
-                  <div className="inner">
-                    <div className="entry tokyo_tm_portfolio_animation_wrap">
-                      <Item
-                        original="/img/portfolio/2.jpg"
-                        thumbnail="/img/portfolio/2.jpg"
-                        width={600}
-                        height={400}
-                      >
-                        {({ ref, open }) => (
-                          <Image
-                            srl_gallery_image="true"
-                            src="/img/portfolio/2.jpg"
-                            alt="Cord Baxter"
-                            data-tip
-                            data-for="cord"
-                            role="button"
-                            ref={ref}
-                            onClick={open}
-                          />
-                        )}
-                      </Item>
-                      <ReactTooltip
-                        id="cord"
-                        place="bottom"
-                        type="light"
-                        effect="float"
-                        className="tooltip-wrapper"
-                      >
-                        <div>
-                          <h5>Cord Baxter</h5>
-                          <span></span>
-                        </div>
-                      </ReactTooltip>
-                    </div>
-                  </div>
-                </li>
-                {/* END SHOT */}
-
-              
-
+                
+                {/* START OF IMAGE 3 - KYLE FIELD */}
                 <li>
                   <div className="inner">
                     <div className="entry tokyo_tm_portfolio_animation_wrap">
                       <Item
                         original="/img/portfolio/3.jpg"
                         thumbnail="/img/portfolio/3.jpg"
-                        width={600}
-                        height={400}
+                        width={1200}
+                        height={800}
                       >
                         {({ ref, open }) => (
                           <Image
@@ -176,44 +378,14 @@ const Portfolio = () => {
                 </li>
                 {/* END SHOT */}
 
-                <li>
-                  <div className="inner">
-                    <div className="entry tokyo_tm_portfolio_animation_wrap">
-                      <Item
-                        original="/img/portfolio/7.jpg"
-                        thumbnail="/img/portfolio/7.jpg"
-                        width={600}
-                        height={400}
-                      >
-                        {({ ref, open }) => (
-                          <Image
-                            srl_gallery_image="true"
-                            src="/img/portfolio/7.jpg"
-                            alt="Childhood"
-                            data-tip
-                            data-for="brendan"
-                            role="button"
-                            ref={ref}
-                            onClick={open}
-                          />
-                        )}
-                      </Item>
-                      <ReactTooltip
-                        id="brendan"
-                        place="bottom"
-                        type="light"
-                        effect="float"
-                        className="tooltip-wrapper"
-                      >
-                        <div>
-                          <h5>Brendan Williams</h5>
-                          <span></span>
-                        </div>
-                      </ReactTooltip>
-                    </div>
-                  </div>
-                </li>
-                {/* END SHOT */}
+
+
+
+
+
+
+
+
               </ul>
             </TabPanel>
             {/* END ALL PORTFOLIO */}
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -21,7 +21,7 @@ const index = () => {
                   <div className="title_flex">
                     <div className="left">
                       <span>Portfolio</span>
-                      <h3>Creative Portfolio</h3>
+                      <h3>Photography Portfolio</h3>
                     </div>
                   </div>
                 </div>
