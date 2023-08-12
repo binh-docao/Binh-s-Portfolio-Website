eb0710d65450310ac6e3e41a61c4565c598f150a
binhdocao <binhdocao@tamu.edu>
2023-08-12 17:07:20 -0500

    page title

--- pages/zta-glow.js
+++ pages/zta-glow.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
+import Seo from "../components/Seo";
 import { useRouter } from "next/router";
 
 const ZTA_GLOW = () => {
+  <Seo pageTitle="ZTA Glow Pictures" />
   const router = useRouter();
   const redirectLink = "https://drive.google.com/drive/folders/1JdGumeuFHwPI9Hba8PKUWy_y1cMuj_Ki?usp=drive_link";
   useEffect(() => {
