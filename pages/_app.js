5aac26ec72d88d221c3ec1828ab2f28917264354
binhdocao <binhdocao@tamu.edu>
2023-08-13 07:07:30 -0500

    use analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ import "../styles/scss/style.scss";
 import "aos/dist/aos.css";
 import { ThemeProvider } from "next-themes";
 import ThemeSwitch from "../components/switch/ThemeSwitch";
+import { Analytics } from '@vercel/analytics/react';
+ 
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -20,6 +22,7 @@ export default function App({ Component, pageProps }) {
       <ThemeProvider attribute="class" defaultTheme="light">
         <ThemeSwitch /> {/* Render ThemeSwitch on every page */}
         <Component {...pageProps} /> {/* Main next component pageprops */}
+        <Analytics />
       </ThemeProvider>
 
       <ToastContainer /> {/* End toast container for email send notification */}
