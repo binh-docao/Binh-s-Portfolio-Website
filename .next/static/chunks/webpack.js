292eaeffe4c637a6680e80e0131ba7c0c763c7df
binhdocao <binhdocao@tamu.edu>
2023-08-19 11:28:48 -0500

    Mobile screen text size force

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8487b187c9da4657"; }
+/******/ 		__webpack_require__.h = function() { return "af913bc85fa898a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
