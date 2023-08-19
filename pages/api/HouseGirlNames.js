f7a1f9c6a8aa6931fee1ec7db311c71d345e8daf
binhdocao <binhdocao@tamu.edu>
2023-08-18 20:19:58 -0500

    CORS 4

---
+++ pages/api/HouseGirlNames.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+    try {
+        const houseGirls = await HouseGirl.find({}, 'name');  // Select only the 'name' field from the HouseGirl model
+        res.status(200).send(houseGirls);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+  }
