5cacf6be1005d21ab10ac4f79ae983eaf4d31a17
binhdocao <binhdocao@tamu.edu>
2023-09-30 09:23:38 -0500

    modify seo

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
 
-const Seo = ({ pageTitle }) => {
+const Seo = ({ pageTitle, imageUrl }) => {
   return (
     <Head>
-      <title>
-        {pageTitle &&
-          `${pageTitle} | Binh Do-Cao`}
-      </title>
+      <title>{pageTitle && `${pageTitle} | Binh Do-Cao`}</title>
+
+      {/* Open Graph tags */}
+      {pageTitle && <meta property="og:title" content={`${pageTitle} | Binh Do-Cao`} />}
+      {imageUrl && <meta property="og:image" content={imageUrl} />}
+      {/* If you have a specific URL for the page, you can also add: */}
+      {/* <meta property="og:url" content={pageUrl} /> */}
     </Head>
   );
 };
