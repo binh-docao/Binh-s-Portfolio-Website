f9537a99112106bc53d0f9e9671c5e2cc1e6eaed
binhdocao <binhdocao@tamu.edu>
2023-10-04 17:56:42 -0500

    big man

---
+++ pages/bmoc.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1HcHLaE6LzGpQ8plAPzNGIG6sY84PE1j1?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Big Man on Campus"
+  imageUrl="/img/portfolio/previews/bmoc.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
