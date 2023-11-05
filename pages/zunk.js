163e5409dcddae26ec7ed9f9bbeaa4b60835a442
binhdocao <binhdocao@tamu.edu>
2023-11-05 11:36:12 -0600

    zta dunk

---
+++ pages/zunk.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1rjW-b8Ob8gJMF__d3mChc9DfthRtHYcr?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Ring Zunk"
+  imageUrl="/img/portfolio/previews/zunk.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
