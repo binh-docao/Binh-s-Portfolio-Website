e7bf2ebd7022f2ea17d1f9b234d2ef2773b553f7
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2023-11-07 12:17:55 -0600

    Add files via upload

---
+++ pages/housegirls.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1o6QGb37nO5cGOQT0ji6KWKQC1JF37r2N?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="House Girls"
+  imageUrl="/img/portfolio/previews/housegirls.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
