be2e15b9a17ef3ffc8feed4b5888092d96bba5c2
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2023-11-13 11:32:21 -0600

    Create surpriseme.js

---
+++ pages/surpriseme.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://forms.gle/fAGXC35wKCUMarqW7";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Surprise!"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
