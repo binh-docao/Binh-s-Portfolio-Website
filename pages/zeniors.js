3c5ab47446906ed2e93785ec88498fe1ea3330c1
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2023-11-17 19:14:39 -0600

    Create zeniors.js

---
+++ pages/zeniors.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1YsjXw5PNPCwvRPJphVKE-9I8Twow3D5a?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="ZTA Senior Date Party"
+  imageUrl="/img/portfolio/previews/zenior.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
