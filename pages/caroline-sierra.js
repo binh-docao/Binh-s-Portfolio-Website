37c098ebe74d8074c782a7fe7c1693dff5ff14dd
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2023-11-20 10:48:51 -0600

    Create caroline-sierra.js

---
+++ pages/caroline-sierra.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1mlbngTlzdmLDD47EcglFYNJ7SYV4GDFj?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Caroline & Sierra"
+  imageUrl="/img/portfolio/previews/caroline-sierra.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
