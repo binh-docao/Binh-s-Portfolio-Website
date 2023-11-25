37b94d4c47dccc3e8e040bc49dbd834393708b3f
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2023-11-25 11:53:07 -0600

    Create kendall-maple.js

---
+++ pages/kendall-maple.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/14E7as9yQ4Vb10S2qc-pYeCUBmCCBg36C?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Kendall & Maple"
+  imageUrl="/img/portfolio/previews/maple.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
