e760ec38525b0e5112e46acaa7fea986c780b78c
binhdocao <binhdocao@tamu.edu>
2023-12-09 14:36:12 -0600

    418

---
+++ pages/418.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1kPb5qXJ2IDqtjIN8FHF4Ja4l4Oew3fVi?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="418 Christmas"
+  imageUrl="/img/portfolio/previews/418.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
