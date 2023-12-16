bb78bc2aad21499b643b02cb80bc980366c654fb
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2023-12-16 17:20:28 -0600

    Create haileyschaefer.js

---
+++ pages/haileyschaefer.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1P9EWIrADrtbsTOcSmHRM2jBZi__nYgWA?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Hailey's First Marathon"
+  imageUrl="/img/portfolio/previews/marathon.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
