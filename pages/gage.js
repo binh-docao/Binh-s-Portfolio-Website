e5790aa257a934ed835b123d1eb4ae06df660223
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2023-12-28 15:37:50 -0600

    Create gage.js

---
+++ pages/gage.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1i7i8-uO2nvyYW1NIWavIshHpepnE-m8x?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Gage Broberg - Graduation"
+  imageUrl="/img/portfolio/previews/gage.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
