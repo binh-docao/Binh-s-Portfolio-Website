be2c282fd342f6df25375a7177af0b0e6990578e
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2024-01-31 17:42:11 -0600

    Create sam.js

---
+++ pages/sam.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1B9V1PvEzd2atGMxbmg3kX2-ArGX_-2fm?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Sam & Skylan"
+  imageUrl="/img/portfolio/previews/ss.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
