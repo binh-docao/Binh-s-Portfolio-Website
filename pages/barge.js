59861785712ed915270d9b29efaceccf7ea5239e
Binh Do-Cao <98185593+binhdocao@users.noreply.github.com>
2024-05-19 17:34:09 -0500

    Create barge.js

---
+++ pages/barge.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Seo from "../components/Seo";
+import { useRouter } from "next/router";
+
+const RING_DAY = () => {
+  const router = useRouter();
+  const redirectLink =
+    "https://drive.google.com/drive/folders/1RfQhTjBGL5ymE_XpVSp3ykFkmBn_E6zL?usp=sharing";
+  useEffect(() => {
+    router.push(redirectLink);
+  }, [router, redirectLink]);
+
+  return null; // Render nothing in the component since it will immediately redirect
+};
+
+const SeoWithCustomTitle = () => (
+  <Seo pageTitle="Barge 2024"
+  imageUrl="/img/portfolio/previews/barge.jpg"></Seo>
+);
+
+const RING_DAY_PAGE = () => (
+  <>
+    <SeoWithCustomTitle />
+    <RING_DAY />
+  </>
+);
+
+export default RING_DAY_PAGE;
